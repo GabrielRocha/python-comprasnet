df1d3a7c757332dd0d58d64e2b2b856931178212
Gabriel Rocha <gabrielrocha@catho.com.br>
2018-05-21 02:31:52 -0300

    Creating a js parser for isolate the code

--- comprasnet/pages/auction_minutes.py
+++ comprasnet/pages/auction_minutes.py
@@ -1,21 +1,22 @@
 from urllib.parse import urlencode
+from comprasnet.parser.javascript import JavaScriptParser
 from . import BaseDetail, log
 from bs4 import BeautifulSoup
 import requests
 import json
-import re
 
 
 class AtaPregao(BaseDetail):
 
-    REGEX_JS = re.compile("^javascript\:(\w*)\(([\w*| |\,|]*)\)\;$")
     ROOT_URL = "http://comprasnet.gov.br/livre/pregao/"
     DETAIL_URL = "{}ata2.asp".format(ROOT_URL)
 
     def __init__(self, co_no_uasg, numprp):
         self.co_no_uasg = co_no_uasg
         self.numprp = numprp
-        self.bs_object = BeautifulSoup(self.get_data(), "html.parser")
+        self._data = self.get_data()
+        self.bs_object = BeautifulSoup(self._data, "html.parser")
+        self.js_parser = JavaScriptParser(self._data)
 
     @property
     def url(self):
@@ -37,102 +38,16 @@ class AtaPregao(BaseDetail):
         return response.text
 
     def scrap_data(self):
-        result_per_provider = "{}{}".format(self.ROOT_URL, self.get_link("btnResultadoFornecr"))
-        declaration = "{}{}".format(self.ROOT_URL, self.get_link("btnDeclaracoes"))
-        return {"result_per_provider": result_per_provider,
-                "declaration": declaration}
+        return {"result_per_provider": self.get_result_per_provider_url(),
+                "declaration": self.get_declaration_url()}
 
-    def _find_js_code(self):
-        try:
-            return self.bs_object.find_all('script', src=lambda x: x is None)[0].text
-        except IndexError:
-            log.error()
-            return []
+    def get_result_per_provider_url(self):
+        return "{}{}".format(self.ROOT_URL,
+                             self.js_parser.get_onclick_function_link("btnResultadoFornecr"))
 
-    def _find_js_function(self, function_name):
-        js_code = self._find_js_code()
-        begin = js_code.find("function {}(".format(function_name))
-        br = js_code[begin:].find("{") + 1
-        while True:
-            next_br = js_code[begin + br:].find("{")
-            next_end = js_code[begin + br:].find("}")
-            if next_end <= next_br or (next_br or next_end) == -1:
-                end = begin + br + next_end + 1
-                break
-            else:
-                br += next_end + 1
-        return js_code[begin: end]
-
-    def find_link_inside_function(self, function_name):
-        return self.get_href_ref(function_name) or self.get_windows_open_ref(function_name)
-
-    def _build_parameters(self, function_name, parameters):
-        try:
-            regex = r"function[| ]*(\w*)\(([\w*| |\,|]*)\)"
-            text = self._find_js_function(function_name)
-            groups = re.search(regex, text).groups()[1]
-            parameters_names = [parameter.strip() for parameter in groups.split(",")]
-            return dict(zip(parameters_names, parameters))
-        except IndexError:
-            return {}
-
-    def _create_url_with_parameters(self, url, parameters):
-        for name, value in parameters.items():
-            regex = "\+[ |]*({})".format(name)
-            url = re.sub(regex, str(value), url)
-        return re.sub(r"\'|\"|\+| ", "", url)
-
-    def get_href_ref(self, function_name):
-        try:
-            text = self._find_js_function(function_name)
-            regex = r'document\.location\.href[ |]*\=[ |]*([\w|\.\?|\=|\+| |\"|\&]*)'
-            match = re.search(regex, text, re.MULTILINE)
-            url = match.groups()[0]
-            if url == "url":
-                return self.get_url_by_variable(text)
-            return url
-        except (IndexError, AttributeError):
-            return
-
-    def get_windows_open_ref(self, function_name):
-        try:
-            text = self._find_js_function(function_name)
-            match = re.search(r'window\.open\(([ |]*([\w|\.\?|\=|\+| |\"|\'|\&]*))',
-                              text, re.MULTILINE)
-            url = match.groups()[0]
-            if url == "url":
-                return self.get_url_by_variable(text)
-            return url
-        except (IndexError, AttributeError):
-            return
-
-    def get_url_by_variable(self, text):
-        try:
-            match = re.search(r'url[ |]*\=([ |]*([\w|\.\?|\=|\+| |\"|\'|\&]*))',
-                              text, re.MULTILINE)
-            return match.groups()[0].strip()
-        except IndexError:
-            return
-
-    def _clean_onlick_function(self, function):
-        try:
-            groups = self.REGEX_JS.match(function).groups()
-            function_name = groups[0]
-            parameters = [parameter.strip() for parameter in groups[-1].split(",")]
-            return {"name": function_name, "parameters": parameters}
-        except AttributeError:
-            return {"name": "", "parameters": ""}
-
-    def get_link(self, id):
-        element = self.bs_object.find(id=id)
-        try:
-            function = self._clean_onlick_function(element['onclick'])
-            url = self.find_link_inside_function(function['name'])
-            dict_parameters = self._build_parameters(function['name'],
-                                                     function['parameters'])
-            return self._create_url_with_parameters(url, dict_parameters)
-        except KeyError:
-            log.error()
+    def get_declaration_url(self):
+        return "{}{}".format(self.ROOT_URL,
+                             self.js_parser.get_onclick_function_link("btnDeclaracoes"))
 
     def to_json(self):
         return json.dumps({})
--- tests/unit/test_class_atapregao.py
+++ tests/unit/test_class_atapregao.py
@@ -1,24 +1,4 @@
-from collections import namedtuple
-
-from bs4 import BeautifulSoup
-from comprasnet.pages.auction_minutes import AtaPregao
-from unittest import mock
 import requests_mock
-import pytest
-import os
-
-
-@mock.patch('comprasnet.pages.auction_minutes.requests.get')
-@pytest.fixture
-def auction_minute(get):
-    path = os.path.join(os.path.dirname(os.path.abspath(__file__)),
-                        '../assets/result_minutes_auction.html')
-    MockResponse = namedtuple('Response', 'status_code, text')
-    MockResponse.status_code = 200
-    with open(path) as html:
-        MockResponse.text = html.read()
-    get.return_value = MockResponse
-    return AtaPregao(1234, 987)
 
 
 def test_to_json_should_return_a_dict(auction_minute):
@@ -41,76 +21,6 @@ def test_get_data(auction_minute):
         assert auction_minute.get_data() == "Success"
 
 
-def test_build_parameters(auction_minute):
-    parameters = auction_minute._build_parameters("ValidaCodigo", [1, 2])
-    assert len(parameters) == 2
-    assert parameters['NomeArquivo'] == 1
-    assert parameters['coduasg'] == 2
-
-
-def test_clean_onlick_function(auction_minute):
-    text = "javascript:resultadoFornecedor(712965);"
-    result = auction_minute._clean_onlick_function(text)
-    assert result['name'] == "resultadoFornecedor"
-    assert result['parameters'] == ["712965"]
-
-
-def test_clean_onlick_function_multiple_parameters(auction_minute):
-    text = "javascript:resultadoFornecedor(712965, 123, 9);"
-    result = auction_minute._clean_onlick_function(text)
-    assert result['name'] == "resultadoFornecedor"
-    assert result['parameters'] == ["712965", "123", "9"]
-
-
-def test_find_js_function(auction_minute):
-    result = auction_minute._find_js_function("teste")
-    expected = 'function teste() { if(true){document.location.href = "Here?parameter=" + coduasg;}}'
-    assert result == expected
-
-
-def test_find_link_inside_function(auction_minute):
-    result = auction_minute.find_link_inside_function("teste")
-    expected = '"Here?parameter=" + coduasg'
-    assert result == expected
-
-
-def test_get_href_ref(auction_minute):
-    result = auction_minute.get_href_ref("teste")
-    expected = '"Here?parameter=" + coduasg'
-    assert result == expected
-
-
-def test_get_href_url_var_ref(auction_minute):
-    result = auction_minute.get_href_ref("teste_url")
-    expected = '"Here?parameter=" + coduasg'
-    assert result == expected
-
-
-def test_get_windows_open_url_var_ref(auction_minute):
-    result = auction_minute.get_windows_open_ref("teste_open_url")
-    expected = '"here"'
-    assert result == expected
-
-
-def test_get_windows_open_ref(auction_minute):
-    result = auction_minute.get_windows_open_ref("teste_open")
-    expected = '"path"'
-    assert result == expected
-
-
-def test_create_url_with_parameters(auction_minute):
-    url_base = '"Here?parameter=" + coduasg'
-    parameters = {"coduasg": 123}
-    url  = auction_minute._create_url_with_parameters(url_base, parameters)
-    expected = 'Here?parameter=123'
-    assert url == expected
-
-
-def test_get_result_per_provider_link(auction_minute):
-    expected = "FornecedorResultado.asp?prgcod=712965&strTipoPregao=E"
-    assert auction_minute.get_link("btnResultadoFornecr") == expected
-
-
 def test_result_per_provider_scrap_data(auction_minute):
     expected = "http://comprasnet.gov.br/livre/pregao/FornecedorResultado.asp?prgcod=712965&strTipoPregao=E"
     result = auction_minute.scrap_data()
@@ -123,3 +33,15 @@ def test_declaration_scrap_data(auction_minute):
     result = auction_minute.scrap_data()
     assert "declaration" in result
     assert result["declaration"] == expected
+
+
+def test_get_result_per_provider_url(auction_minute):
+    expected = "http://comprasnet.gov.br/livre/pregao/FornecedorResultado.asp?prgcod=712965&strTipoPregao=E"
+    result = auction_minute.get_result_per_provider_url()
+    assert result == expected
+
+
+def test_get_declaration_url(auction_minute):
+    expected = "http://comprasnet.gov.br/livre/pregao/declaracoesProposta.asp?prgCod=712965"
+    result = auction_minute.get_declaration_url()
+    assert result == expected
